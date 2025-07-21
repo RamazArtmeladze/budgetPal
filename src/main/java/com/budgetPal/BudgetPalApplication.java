b92d78737ab98a28e399dc11c207051a8401237d
rartmeladze <ramaz.artmeladze@outlook.com>
2025-07-21 17:58:11 +0400

    Initial commit - project setup

---
+++ src/main/java/com/budgetPal/BudgetPalApplication.java
@@ -0,0 +1,13 @@
+package com.budgetPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budgetPal/BudgetPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
