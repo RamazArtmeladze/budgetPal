ffe06700f540bd095ea4286e002467e85091f3ac
rartmeladze <ramaz.artmeladze@outlook.com>
2025-08-05 04:06:02 +0400

    add user soft delete endpoint

--- src/main/java/com/budgetPal/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/budgetPal/service/impl/TransactionServiceImpl.java
@@ -35,7 +35,6 @@ import static com.budgetPal.utility.MessageConstants.EXPENSE_TYPE_NOT_FOUND_MESS
 import static com.budgetPal.utility.MessageConstants.PAYMENT_TYPE_NOT_FOUND_MESSAGE;
 import static com.budgetPal.utility.MessageConstants.TRANSACTION_NOT_FOUND_MESSAGE;
 import static com.budgetPal.utility.MessageConstants.USER_NOT_FOUND_BY_EMAIL_MESSAGE;
-import static com.budgetPal.utility.MessageConstants.USER_NOT_FOUND_BY_ID_MESSAGE;
 
 @Service
 @RequiredArgsConstructor
